d4cb67100cd457ef563402206dcf9fe8d8ea85d3
AnnetaDe <anneta.liss@gmail.com>
2025-02-02 22:57:35 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -25,3 +25,22 @@ model += juice_bottles + lemonade_bottles, "Maximize Drinks"
 model.solve()
 print(f"Juice bottles: {pulp.value(juice_bottles)}")
 print(f"Lemonade bottles: {pulp.value(lemonade_bottles)}")
+
+
+# Ініціалізація моделі2
+model2 = pulp.LpProblem("Maximize Beverage Production", pulp.LpMaximize)
+# Змінні
+Lemonade = pulp.LpVariable("Lemonade", lowBound=0, cat="Continuous")
+Juice = pulp.LpVariable("Juice", lowBound=0, cat="Continuous")
+# Обмеження ресурсів
+model2 += 2 * Lemonade + 1 * Juice <= 100  # Вода
+model2 += 1 * Lemonade <= 50  # Цукор
+model2 += 1 * Lemonade <= 30  # Лимонний сік
+model2 += 2 * Juice <= 40  # Фруктове пюре
+# Функція максимізації виробництва
+model2 += Lemonade + Juice
+# Розв'язання задачі
+model2.solve()
+# Виведення результатів
+print("Виробити 'Лимонаду':", Lemonade.varValue)
+print("Виробити 'Фруктового соку':", Juice.varValue)
